1af029fe1b79dea4861d1377d4c9ada39053283a
aturdaly <aturdaly@gmail.com>
2020-06-09 04:40:32 +0600

    first commit

---
+++ src/main/java/koschei/config/AppConfig.java
@@ -0,0 +1,31 @@
+package koschei.config;
+
+import koschei.models.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "koschei")
+public class AppConfig {
+
+    @Bean
+    public static Island2 getIsland(Wood3 wood) {
+        return new Island2(wood);
+    }
+
+    @Bean
+    public static Egg6 getEgg() {
+        return new Egg6();
+    }
+
+    @Bean
+    public static Needle7 getNeedle() {
+        return new Needle7();
+    }
+
+    @Bean
+    public static Deth8 getDeth() {
+        return new Deth8();
+    }
+}
---
+++ src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,14 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Needle7 {
+
+    @Autowired
+    private Deth8 deth;
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле :( " + deth.toString();
+    }
+}
